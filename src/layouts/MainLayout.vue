6dd54ccec1e4b9579389f78974c440c6e2dbea21
Johnexzy <obajohn75@gmail.com>
2022-06-04 16:17:18 +0100

    added excerpts

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -118,14 +118,11 @@
               <span class="liner"></span>
             </q-card-section>
             <q-card-section class="q-pa-lg text-center">
-              <div class="fb-page" data-href="https://www.facebook.com/ShekinahAssemblyUnnCampusChurch"
-                data-tabs="timeline" data-width="" data-height="" data-small-header="false"
-                data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
-                <blockquote cite="https://www.facebook.com/ShekinahAssemblyUnnCampusChurch"
-                  class="fb-xfbml-parse-ignore"><a
-                    href="https://www.facebook.com/ShekinahAssemblyUnnCampusChurch">Shekinah Assembly Unn, Campus
-                    Church</a></blockquote>
-              </div>
+              <iframe
+                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FShekinahAssemblyUnnCampusChurch&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+                width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
+                allowfullscreen="true"
+                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
             </q-card-section>
 
           </q-card>
